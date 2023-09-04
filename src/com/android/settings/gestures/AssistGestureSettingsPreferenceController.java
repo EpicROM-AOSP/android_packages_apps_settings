yaap/packages_apps_Settings

Settings: Gestures: Pull out all lone switches
As usual, useless full page and a video for what we already understand from the summary
For a single toggle.... So useless...
Also reorder things. Get rid of all remainants and resources

src/com/android/settings/gestures/AssistGestureSettingsPreferenceController.java
@@ -28,12 +28,12 @@ import androidx.preference.Preference;
 import androidx.preference.PreferenceScreen;
 
 import com.android.settings.R;
+import com.android.settings.core.TogglePreferenceController;
 import com.android.settings.overlay.FeatureFactory;
 
-public class AssistGestureSettingsPreferenceController extends GesturePreferenceController {
+public class AssistGestureSettingsPreferenceController extends TogglePreferenceController {
 
     private static final String TAG = "AssistGesture";
-    private static final String PREF_KEY_VIDEO = "gesture_assist_video";
 
     private static final String SECURE_KEY_ASSIST = ASSIST_GESTURE_ENABLED;
     private static final String SECURE_KEY_SILENCE = ASSIST_GESTURE_SILENCE_ALERTS_ENABLED;
@@ -70,10 +70,7 @@ public class AssistGestureSettingsPreferenceController extends GesturePreference
         mScreen = screen;
         mPreference = screen.findPreference(getPreferenceKey());
         super.displayPreference(screen);
-    }
 
-    @Override
-    public void onStart() {
         if (mWasAvailable != isAvailable()) {
             // Only update the preference visibility if the availability has changed -- otherwise
             // the preference may be incorrectly added to screens with collapsed sections.
@@ -118,11 +115,6 @@ public class AssistGestureSettingsPreferenceController extends GesturePreference
     }
 
     @Override
-    protected String getVideoPrefKey() {
-        return PREF_KEY_VIDEO;
-    }
-
-    @Override
     public CharSequence getSummary() {
         boolean isEnabled = isAssistGestureEnabled() && mFeatureProvider.isSupported(mContext);
         if (!mAssistOnly) {
@@ -135,5 +127,10 @@ public class AssistGestureSettingsPreferenceController extends GesturePreference
     @Override
     public boolean isChecked() {
         return Settings.Secure.getInt(mContext.getContentResolver(), SECURE_KEY_ASSIST, OFF) == ON;
+    }
+
+    @Override
+    public int getSliceHighlightMenuRes() {
+        return R.string.menu_key_yasp;
     }
 }

src/com/android/settings/gestures/DoubleTapScreenPreferenceController.java
@@ -20,20 +20,18 @@ import static android.provider.Settings.Secure.DOZE_DOUBLE_TAP_GESTURE;
 
 import android.annotation.UserIdInt;
 import android.content.Context;
-import android.content.SharedPreferences;
 import android.hardware.display.AmbientDisplayConfiguration;
 import android.os.UserHandle;
 import android.provider.Settings;
 import android.text.TextUtils;
 
-import androidx.annotation.VisibleForTesting;
+import com.android.settings.core.TogglePreferenceController;
+import com.android.settings.R;
 
-public class DoubleTapScreenPreferenceController extends GesturePreferenceController {
+public class DoubleTapScreenPreferenceController extends TogglePreferenceController {
 
     private final int ON = 1;
     private final int OFF = 0;
-
-    private static final String PREF_KEY_VIDEO = "gesture_double_tap_screen_video";
 
     private static final String SECURE_KEY = DOZE_DOUBLE_TAP_GESTURE;
 
@@ -49,17 +47,6 @@ public class DoubleTapScreenPreferenceController extends GesturePreferenceContro
     public DoubleTapScreenPreferenceController setConfig(AmbientDisplayConfiguration config) {
         mAmbientConfig = config;
         return this;
-    }
-
-    public static boolean isSuggestionComplete(Context context, SharedPreferences prefs) {
-        return isSuggestionComplete(new AmbientDisplayConfiguration(context), prefs);
-    }
-
-    @VisibleForTesting
-    static boolean isSuggestionComplete(AmbientDisplayConfiguration config,
-            SharedPreferences prefs) {
-        return !config.doubleTapSensorAvailable()
-                || prefs.getBoolean(DoubleTapScreenSettings.PREF_KEY_SUGGESTION_COMPLETE, false);
     }
 
     @Override
@@ -89,11 +76,6 @@ public class DoubleTapScreenPreferenceController extends GesturePreferenceContro
     }
 
     @Override
-    protected String getVideoPrefKey() {
-        return PREF_KEY_VIDEO;
-    }
-
-    @Override
     public boolean isChecked() {
         return getAmbientConfig().doubleTapGestureEnabled(mUserId);
     }
@@ -103,5 +85,10 @@ public class DoubleTapScreenPreferenceController extends GesturePreferenceContro
             mAmbientConfig = new AmbientDisplayConfiguration(mContext);
         }
         return mAmbientConfig;
+    }
+
+    @Override
+    public int getSliceHighlightMenuRes() {
+        return R.string.menu_key_yasp;
     }
 }

src/com/android/settings/gestures/DoubleTwistPreferenceController.java
@@ -30,13 +30,13 @@ import androidx.annotation.VisibleForTesting;
 
 import com.android.settings.R;
 import com.android.settings.Utils;
+import com.android.settings.core.TogglePreferenceController;
 
-public class DoubleTwistPreferenceController extends GesturePreferenceController {
+public class DoubleTwistPreferenceController extends TogglePreferenceController {
 
     private final int ON = 1;
     private final int OFF = 0;
 
-    private static final String PREF_KEY_VIDEO = "gesture_double_twist_video";
     private final String mDoubleTwistPrefKey;
     private final UserManager mUserManager;
 
@@ -44,11 +44,6 @@ public class DoubleTwistPreferenceController extends GesturePreferenceController
         super(context, key);
         mDoubleTwistPrefKey = key;
         mUserManager = (UserManager) context.getSystemService(Context.USER_SERVICE);
-    }
-
-    public static boolean isSuggestionComplete(Context context, SharedPreferences prefs) {
-        return !isGestureAvailable(context)
-                || prefs.getBoolean(DoubleTwistGestureSettings.PREF_KEY_SUGGESTION_COMPLETE, false);
     }
 
     public static boolean isGestureAvailable(Context context) {
@@ -80,11 +75,6 @@ public class DoubleTwistPreferenceController extends GesturePreferenceController
     @Override
     public boolean isPublicSlice() {
         return true;
-    }
-
-    @Override
-    protected String getVideoPrefKey() {
-        return PREF_KEY_VIDEO;
     }
 
     @Override
@@ -121,4 +111,10 @@ public class DoubleTwistPreferenceController extends GesturePreferenceController
     public static int getManagedProfileId(UserManager userManager) {
         return Utils.getManagedProfileId(userManager, UserHandle.myUserId());
     }
+
+    @Override
+    public int getSliceHighlightMenuRes() {
+        return R.string.menu_key_yasp;
+    }
+
 }

src/com/android/settings/gestures/SwipeToNotificationPreferenceController.java
@@ -23,14 +23,14 @@ import android.content.SharedPreferences;
 import android.provider.Settings;
 import android.text.TextUtils;
 
+import com.android.settings.R;
+import com.android.settings.core.TogglePreferenceController;
 import com.android.settings.Utils;
 
-public class SwipeToNotificationPreferenceController extends GesturePreferenceController {
+public class SwipeToNotificationPreferenceController extends TogglePreferenceController {
 
     private static final int ON = 1;
     private static final int OFF = 0;
-
-    private static final String PREF_KEY_VIDEO = "gesture_swipe_down_fingerprint_video";
 
     private static final String SECURE_KEY = SYSTEM_NAVIGATION_KEYS_ENABLED;
 
@@ -38,21 +38,10 @@ public class SwipeToNotificationPreferenceController extends GesturePreferenceCo
         super(context, key);
     }
 
-    public static boolean isSuggestionComplete(Context context, SharedPreferences prefs) {
-        return !isGestureAvailable(context)
-                || prefs.getBoolean(SwipeToNotificationSettings.PREF_KEY_SUGGESTION_COMPLETE,
-                false);
-    }
-
     private static boolean isGestureAvailable(Context context) {
         return Utils.hasFingerprintHardware(context)
                 && context.getResources()
                 .getBoolean(com.android.internal.R.bool.config_supportSystemNavigationKeys);
-    }
-
-    @Override
-    protected String getVideoPrefKey() {
-        return PREF_KEY_VIDEO;
     }
 
     @Override
@@ -93,4 +82,10 @@ public class SwipeToNotificationPreferenceController extends GesturePreferenceCo
     public static boolean isAvailable(Context context) {
         return isGestureAvailable(context);
     }
+
+    @Override
+    public int getSliceHighlightMenuRes() {
+        return R.string.menu_key_yasp;
+    }
+
 }

src/com/android/settings/gestures/TapScreenGesturePreferenceController.java
@@ -25,9 +25,10 @@ import android.os.SystemProperties;
 import android.os.UserHandle;
 import android.provider.Settings;
 
-public class TapScreenGesturePreferenceController extends GesturePreferenceController {
+import com.android.settings.R;
+import com.android.settings.core.TogglePreferenceController;
 
-    private static final String PREF_KEY_VIDEO = "gesture_tap_screen_video";
+public class TapScreenGesturePreferenceController extends TogglePreferenceController {
 
     private AmbientDisplayConfiguration mAmbientConfig;
     @UserIdInt
@@ -59,11 +60,6 @@ public class TapScreenGesturePreferenceController extends GesturePreferenceContr
     }
 
     @Override
-    protected String getVideoPrefKey() {
-        return PREF_KEY_VIDEO;
-    }
-
-    @Override
     public CharSequence getSummary() {
         return super.getSummary();
     }
@@ -86,5 +82,10 @@ public class TapScreenGesturePreferenceController extends GesturePreferenceContr
             mAmbientConfig = new AmbientDisplayConfiguration(mContext);
         }
         return mAmbientConfig;
+    }
+
+    @Override
+    public int getSliceHighlightMenuRes() {
+        return R.string.menu_key_yasp;
     }
 }

EpicROM-AOSP/android_packages_apps_settings

src/com/android/settings/gestures/AssistGestureSettingsPreferenceController.java
@@ -28,12 +28,12 @@ import androidx.preference.Preference;
 import androidx.preference.PreferenceScreen;
 
 import com.android.settings.R;
+import com.android.settings.core.TogglePreferenceController;
 import com.android.settings.overlay.FeatureFactory;
 
-public class AssistGestureSettingsPreferenceController extends GesturePreferenceController {
+public class AssistGestureSettingsPreferenceController extends TogglePreferenceController {
 
     private static final String TAG = "AssistGesture";
-    private static final String PREF_KEY_VIDEO = "gesture_assist_video";
 
     private static final String SECURE_KEY_ASSIST = ASSIST_GESTURE_ENABLED;
     private static final String SECURE_KEY_SILENCE = ASSIST_GESTURE_SILENCE_ALERTS_ENABLED;
@@ -70,10 +70,7 @@ public class AssistGestureSettingsPreferenceController extends GesturePreference
         mScreen = screen;
         mPreference = screen.findPreference(getPreferenceKey());
         super.displayPreference(screen);
-    }
 
-    @Override
-    public void onStart() {
         if (mWasAvailable != isAvailable()) {
             // Only update the preference visibility if the availability has changed -- otherwise
             // the preference may be incorrectly added to screens with collapsed sections.
@@ -118,11 +115,6 @@ public class AssistGestureSettingsPreferenceController extends GesturePreference
     }
 
     @Override
-    protected String getVideoPrefKey() {
-        return PREF_KEY_VIDEO;
-    }
-
-    @Override
     public CharSequence getSummary() {
         boolean isEnabled = isAssistGestureEnabled() && mFeatureProvider.isSupported(mContext);
         if (!mAssistOnly) {
@@ -135,5 +127,10 @@ public class AssistGestureSettingsPreferenceController extends GesturePreference
     @Override
     public boolean isChecked() {
         return Settings.Secure.getInt(mContext.getContentResolver(), SECURE_KEY_ASSIST, OFF) == ON;
+    }
+
+    @Override
+    public int getSliceHighlightMenuRes() {
+        return R.string.menu_key_epic;
     }
 }

src/com/android/settings/gestures/DoubleTapScreenPreferenceController.java
@@ -20,20 +20,18 @@ import static android.provider.Settings.Secure.DOZE_DOUBLE_TAP_GESTURE;
 
 import android.annotation.UserIdInt;
 import android.content.Context;
-import android.content.SharedPreferences;
 import android.hardware.display.AmbientDisplayConfiguration;
 import android.os.UserHandle;
 import android.provider.Settings;
 import android.text.TextUtils;
 
-import androidx.annotation.VisibleForTesting;
+import com.android.settings.core.TogglePreferenceController;
+import com.android.settings.R;
 
-public class DoubleTapScreenPreferenceController extends GesturePreferenceController {
+public class DoubleTapScreenPreferenceController extends TogglePreferenceController {
 
     private final int ON = 1;
     private final int OFF = 0;
-
-    private static final String PREF_KEY_VIDEO = "gesture_double_tap_screen_video";
 
     private static final String SECURE_KEY = DOZE_DOUBLE_TAP_GESTURE;
 
@@ -49,17 +47,6 @@ public class DoubleTapScreenPreferenceController extends GesturePreferenceContro
     public DoubleTapScreenPreferenceController setConfig(AmbientDisplayConfiguration config) {
         mAmbientConfig = config;
         return this;
-    }
-
-    public static boolean isSuggestionComplete(Context context, SharedPreferences prefs) {
-        return isSuggestionComplete(new AmbientDisplayConfiguration(context), prefs);
-    }
-
-    @VisibleForTesting
-    static boolean isSuggestionComplete(AmbientDisplayConfiguration config,
-            SharedPreferences prefs) {
-        return !config.doubleTapSensorAvailable()
-                || prefs.getBoolean(DoubleTapScreenSettings.PREF_KEY_SUGGESTION_COMPLETE, false);
     }
 
     @Override
@@ -89,11 +76,6 @@ public class DoubleTapScreenPreferenceController extends GesturePreferenceContro
     }
 
     @Override
-    protected String getVideoPrefKey() {
-        return PREF_KEY_VIDEO;
-    }
-
-    @Override
     public boolean isChecked() {
         return getAmbientConfig().doubleTapGestureEnabled(mUserId);
     }
@@ -103,5 +85,10 @@ public class DoubleTapScreenPreferenceController extends GesturePreferenceContro
             mAmbientConfig = new AmbientDisplayConfiguration(mContext);
         }
         return mAmbientConfig;
+    }
+
+    @Override
+    public int getSliceHighlightMenuRes() {
+        return R.string.menu_key_epic;
     }
 }

src/com/android/settings/gestures/DoubleTwistPreferenceController.java
@@ -30,13 +30,13 @@ import androidx.annotation.VisibleForTesting;
 
 import com.android.settings.R;
 import com.android.settings.Utils;
+import com.android.settings.core.TogglePreferenceController;
 
-public class DoubleTwistPreferenceController extends GesturePreferenceController {
+public class DoubleTwistPreferenceController extends TogglePreferenceController {
 
     private final int ON = 1;
     private final int OFF = 0;
 
-    private static final String PREF_KEY_VIDEO = "gesture_double_twist_video";
     private final String mDoubleTwistPrefKey;
     private final UserManager mUserManager;
 
@@ -44,11 +44,6 @@ public class DoubleTwistPreferenceController extends GesturePreferenceController
         super(context, key);
         mDoubleTwistPrefKey = key;
         mUserManager = (UserManager) context.getSystemService(Context.USER_SERVICE);
-    }
-
-    public static boolean isSuggestionComplete(Context context, SharedPreferences prefs) {
-        return !isGestureAvailable(context)
-                || prefs.getBoolean(DoubleTwistGestureSettings.PREF_KEY_SUGGESTION_COMPLETE, false);
     }
 
     public static boolean isGestureAvailable(Context context) {
@@ -80,11 +75,6 @@ public class DoubleTwistPreferenceController extends GesturePreferenceController
     @Override
     public boolean isPublicSlice() {
         return true;
-    }
-
-    @Override
-    protected String getVideoPrefKey() {
-        return PREF_KEY_VIDEO;
     }
 
     @Override
@@ -121,4 +111,10 @@ public class DoubleTwistPreferenceController extends GesturePreferenceController
     public static int getManagedProfileId(UserManager userManager) {
         return Utils.getManagedProfileId(userManager, UserHandle.myUserId());
     }
+
+    @Override
+    public int getSliceHighlightMenuRes() {
+        return R.string.menu_key_epic;
+    }
+
 }

src/com/android/settings/gestures/SwipeToNotificationPreferenceController.java
@@ -23,14 +23,14 @@ import android.content.SharedPreferences;
 import android.provider.Settings;
 import android.text.TextUtils;
 
+import com.android.settings.R;
+import com.android.settings.core.TogglePreferenceController;
 import com.android.settings.Utils;
 
-public class SwipeToNotificationPreferenceController extends GesturePreferenceController {
+public class SwipeToNotificationPreferenceController extends TogglePreferenceController {
 
     private static final int ON = 1;
     private static final int OFF = 0;
-
-    private static final String PREF_KEY_VIDEO = "gesture_swipe_down_fingerprint_video";
 
     private static final String SECURE_KEY = SYSTEM_NAVIGATION_KEYS_ENABLED;
 
@@ -38,21 +38,10 @@ public class SwipeToNotificationPreferenceController extends GesturePreferenceCo
         super(context, key);
     }
 
-    public static boolean isSuggestionComplete(Context context, SharedPreferences prefs) {
-        return !isGestureAvailable(context)
-                || prefs.getBoolean(SwipeToNotificationSettings.PREF_KEY_SUGGESTION_COMPLETE,
-                false);
-    }
-
     private static boolean isGestureAvailable(Context context) {
         return Utils.hasFingerprintHardware(context)
                 && context.getResources()
                 .getBoolean(com.android.internal.R.bool.config_supportSystemNavigationKeys);
-    }
-
-    @Override
-    protected String getVideoPrefKey() {
-        return PREF_KEY_VIDEO;
     }
 
     @Override
@@ -93,4 +82,10 @@ public class SwipeToNotificationPreferenceController extends GesturePreferenceCo
     public static boolean isAvailable(Context context) {
         return isGestureAvailable(context);
     }
+
+    @Override
+    public int getSliceHighlightMenuRes() {
+        return R.string.menu_key_epic;
+    }
+
 }

src/com/android/settings/gestures/TapScreenGesturePreferenceController.java
@@ -25,9 +25,10 @@ import android.os.SystemProperties;
 import android.os.UserHandle;
 import android.provider.Settings;
 
-public class TapScreenGesturePreferenceController extends GesturePreferenceController {
+import com.android.settings.R;
+import com.android.settings.core.TogglePreferenceController;
 
-    private static final String PREF_KEY_VIDEO = "gesture_tap_screen_video";
+public class TapScreenGesturePreferenceController extends TogglePreferenceController {
 
     private AmbientDisplayConfiguration mAmbientConfig;
     @UserIdInt
@@ -59,11 +60,6 @@ public class TapScreenGesturePreferenceController extends GesturePreferenceContr
     }
 
     @Override
-    protected String getVideoPrefKey() {
-        return PREF_KEY_VIDEO;
-    }
-
-    @Override
     public CharSequence getSummary() {
         return super.getSummary();
     }
@@ -86,5 +82,10 @@ public class TapScreenGesturePreferenceController extends GesturePreferenceContr
             mAmbientConfig = new AmbientDisplayConfiguration(mContext);
         }
         return mAmbientConfig;
+    }
+
+    @Override
+    public int getSliceHighlightMenuRes() {
+        return R.string.menu_key_epic;
     }
 }
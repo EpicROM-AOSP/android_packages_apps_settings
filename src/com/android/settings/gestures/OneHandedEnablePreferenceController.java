yaap/packages_apps_Settings

Settings: Adapt our preferences and controllers to Sv2

src/com/android/settings/gestures/OneHandedEnablePreferenceController.java
@@ -97,4 +97,9 @@ public class OneHandedEnablePreferenceController extends TogglePreferenceControl
     public void onStop() {
         mOneHandedUtils.unregisterToggleAwareObserver();
     }
+
+    @Override
+    public int getSliceHighlightMenuRes() {
+        return R.string.menu_key_yasp;
+    }
 }

src/com/android/settings/gestures/PickupGesturePreferenceController.java
@@ -26,6 +26,7 @@ import android.provider.Settings;
 import android.text.TextUtils;
 
 import com.android.settings.core.TogglePreferenceController;
+import com.android.settings.R;
 
 public class PickupGesturePreferenceController extends TogglePreferenceController {
 
@@ -93,5 +94,10 @@ public class PickupGesturePreferenceController extends TogglePreferenceControlle
         }
 
         return mAmbientConfig;
+    }
+
+    @Override
+    public int getSliceHighlightMenuRes() {
+        return R.string.menu_key_yasp;
     }
 }

EpicROM-AOSP/android_packages_apps_settings

src/com/android/settings/gestures/OneHandedEnablePreferenceController.java
@@ -111,4 +111,9 @@ public class OneHandedEnablePreferenceController extends TogglePreferenceControl
             refreshSummary(mPreference);
         }
     }
+
+    @Override
+    public int getSliceHighlightMenuRes() {
+        return R.string.menu_key_epic;
+    }
 }

src/com/android/settings/gestures/PickupGesturePreferenceController.java
@@ -26,6 +26,7 @@ import android.provider.Settings;
 import android.text.TextUtils;
 
 import com.android.settings.core.TogglePreferenceController;
+import com.android.settings.R;
 
 public class PickupGesturePreferenceController extends TogglePreferenceController {
 
@@ -93,5 +94,10 @@ public class PickupGesturePreferenceController extends TogglePreferenceControlle
         }
 
         return mAmbientConfig;
+    }
+
+    @Override
+    public int getSliceHighlightMenuRes() {
+        return R.string.menu_key_epic;
     }
 }
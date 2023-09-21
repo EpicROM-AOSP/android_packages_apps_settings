Project-Xtended/packages_apps_Xtensions

Navigation: disable nav bar options when gestural navbar is active
Signed-off-by: SuperDroidBond <superdroidbond@yahoo.com>

src/com/xtended/fragments/NavbarSettings.java
@@ -42,10 +42,13 @@ import android.provider.Settings;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.internal.logging.MetricsLogger;
 import com.android.internal.logging.nano.MetricsProto;
+import com.android.internal.util.xtended.XtendedUtils;
 import com.android.settings.R;
 
 public class NavbarSettings extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
+
+    private static final String CATEGORY_3BUTTONS = "button_navigation_settings_page";
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -53,6 +56,13 @@ public class NavbarSettings extends SettingsPreferenceFragment implements
         addPreferencesFromResource(R.xml.x_settings_navigation);
 
         final PreferenceScreen prefScreen = getPreferenceScreen();
+
+        PreferenceCategory navBarCat = (PreferenceCategory) prefScreen
+                .findPreference(CATEGORY_3BUTTONS);
+
+        if (!XtendedUtils.isThemeEnabled("com.android.internal.systemui.navbar.threebutton")) {
+            navBarCat.setEnabled(false);
+        }
     }
 
     @Override

EpicROM-AOSP/android_packages_apps_settings

src/com/epic/fragments/NavbarSettings.java
@@ -41,10 +41,13 @@ import android.provider.Settings;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.internal.logging.MetricsLogger;
 import com.android.internal.logging.nano.MetricsProto;
+import com.android.internal.util.epic.EpicUtils;
 import com.android.settings.R;
 
 public class NavbarSettings extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
+
+    private static final String CATEGORY_3BUTTONS = "button_navigation_settings_page";
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -52,6 +55,13 @@ public class NavbarSettings extends SettingsPreferenceFragment implements
         addPreferencesFromResource(R.xml.anatolia_settings_navigation);
 
         final PreferenceScreen prefScreen = getPreferenceScreen();
+
+        PreferenceCategory navBarCat = (PreferenceCategory) prefScreen
+                .findPreference(CATEGORY_3BUTTONS);
+
+        if (!EpicUtils.isThemeEnabled("com.android.internal.systemui.navbar.threebutton")) {
+            navBarCat.setEnabled(false);
+        }
     }
 
     @Override